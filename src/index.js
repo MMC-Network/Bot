5b25fe80cde321dc81b405c84661a35ec76f3994
JBecconsall <joshuabecconsall05@gmail.com>
2023-10-17 17:55:50 +0100

    command handler setup

--- src/index.js
+++ src/index.js
@@ -21,6 +21,14 @@ module.exports = { client }
 
 client.on('ready', async () => {
     console.log('The bot is online')
+
+    new CH({
+        client,
+        commandsDir: path.join(__dirname, 'commands'),
+        featuresDir: path.join(__dirname, 'events'),
+        testServers: ['1026962943616753735'],
+        botOwners: ['818539885950009454', '306506629115805696', '448147561019080705'] // J - R - C
+    })
 })
 
 client.login('OTgyMjQ0OTY5OTE2MDMxMDE2.GbhO4w.7kDw7rsy28_d_iDsBL3MufXrc-b83nG6n-aYNM')
