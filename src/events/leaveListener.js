f22978d90e79f1bccd43f5bc589a4b53fc78ad47
JBecconsall <joshuabecconsall05@gmail.com>
2023-10-23 16:59:07 +0100

    leave listener and icon on ticket panel

---
+++ src/events/leaveListener.js
@@ -0,0 +1,20 @@
+const { EmbedBuilder, MessagePayload } = require('discord.js');
+const { client } = require('../index');
+
+module.exports = async(interaction) => {
+
+    client.on('guildMemberRemove', async(interaction) => {
+
+
+        let channel = interaction.guild.channels.cache.find(c => c.id === '1166037339949236234');
+
+        //let message = MessagePayload.create(channel, {
+          //  embeds: [embed]
+       // });
+
+       await channel.send(`<@${interaction.user.id}> has left the discord`)
+
+    
+    })
+
+}
