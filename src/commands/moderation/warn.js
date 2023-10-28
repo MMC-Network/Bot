694753ef2f3917feecbe228036b215e2f6536726
JBecconsall <joshuabecconsall05@gmail.com>
2023-10-28 18:05:58 +0100

    warn command

---
+++ src/commands/moderation/warn.js
@@ -0,0 +1,118 @@
+const {
+    EmbedBuilder,
+    Embed,
+    MessagePayload,
+    ApplicationCommandOptionType
+} = require("discord.js");
+const {
+    CommandType
+} = require("wokcommands");
+
+module.exports = {
+    catgeory: 'Moderation',
+    name: 'warn',
+    description: 'Log a warning for a user',
+    testOnly: true,
+    type: CommandType.SLASH,
+    maxArgs: 2,
+    options: [{
+            name: 'user',
+            description: 'The user to warn',
+            required: true,
+            type: ApplicationCommandOptionType.User
+        },
+        {
+            name: 'reason',
+            description: 'The reason for warning the user',
+            required: true,
+            type: ApplicationCommandOptionType.String
+        }
+    ],
+    callback: async ({
+        interaction,
+        client
+    }) => {
+
+        const user = interaction.options.getUser('user');
+        const reason = interaction.options.getString('reason');
+
+        let logChannel = interaction.guild.channels.cache.get('1166039509855653998');
+
+        const success = new EmbedBuilder()
+            .setTitle("User Successfully Warned")
+            .setDescription(`<:greentick:1116854720515018824> <@${user.id}> has been warned`)
+            .addFields({
+                name: 'Username',
+                value: `\`${user.tag}\``,
+                inline: true
+            }, {
+                name: 'User ID',
+                value: `\`${user.id}\``,
+                inline: true
+            }, {
+                name: 'Warned By',
+                value: `<@${interaction.user.id}>`,
+                inline: true
+            }, {
+                name: "Warning Reason",
+                value: "```" + ` ${reason.toString()} ` + "```",
+                inline: false
+            })
+            .setThumbnail(interaction.user.avatarURL())
+            .setTimestamp()
+            .setColor('#f01d1d')
+            .setFooter({
+                text: `Executed by ${interaction.user.tag}`
+            })
+
+        const logEmbed = new EmbedBuilder()
+            .setTitle("NEW WARNING")
+            .setDescription(`<:greentick:1116854720515018824> <@${user.id}> has been warned in ${interaction.guild.name}`)
+            .addFields({
+                name: 'Username',
+                value: `\`${user.tag}\``,
+                inline: true
+            }, {
+                name: 'User ID',
+                value: `\`${user.id}\``,
+                inline: true
+            }, {
+                name: 'Warned By',
+                value: `<@${interaction.user.id}>`,
+                inline: true
+            }, {
+                name: "Warning Reason",
+                value: "```" + ` ${reason.toString()} ` + "```",
+                inline: false
+            })
+            .setThumbnail(interaction.user.avatarURL())
+            .setTimestamp()
+            .setColor('#f01d1d')
+            .setFooter({
+                text: `Executed by ${interaction.user.tag}`
+            })
+
+        let logMsg = MessagePayload.create(logChannel, {
+            embeds: [logEmbed]
+        })
+
+        let successMsg = MessagePayload.create(interaction.channel, {
+            embeds: [success]
+        })
+        if (!interaction.member.roles.cache.has('1163568181399535688')) {
+
+            interaction.reply({
+                content: 'You do not have permission to warn users',
+                ephemeral: true
+            })
+
+        } else {
+
+            logChannel.send(logMsg)
+
+            await interaction.reply(successMsg)
+
+
+        }
+    }
+}
